f37df6193ff6791b5fdff6b2c2868c2f59a267f4
lfvm <lfvm200@gmail.com>
2022-11-15 19:40:29 -0600

    renders product info on card

--- web-app/src/components/ui/ProductCard.tsx
+++ web-app/src/components/ui/ProductCard.tsx
@@ -1,22 +1,31 @@
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import Button from "@mui/material/Button";
+import { Product } from "../../types/product";
 
-function ProductCard(): JSX.Element {
+interface PageLayoutInterface {
+  product: Product;
+}
+
+function ProductCard(props: PageLayoutInterface): JSX.Element {
+  const { product } = props;
+
   return (
-    <div className="product_card px-4 bg-white w-[200px] h-fit shadow-sm flex flex-col items-center justify-between hover:shadow-md mb-4 rounded-lg">
+    <div className="product_card px-4 bg-white w-[200px] h-[400px] shadow-sm flex flex-col items-center justify-between hover:shadow-md mb-4 rounded-lg">
       {/* Img */}
       <img
-        src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
+        src={`${product.image}`}
         alt="product"
-        className="lg:w-full lg:h-full w-[150px] h-[150px] object-contain mt-4"
+        className="lg:w-[200px] lg:[200px] w-[150px] h-[150px] object-contain my-4"
       />
 
       {/* Info */}
-      <div className="info px-2 py-4">
-        <h2 className="text-[15px] font-medium overflow-hidden max-h-9 text-b3">
-          Very Cool Product
+      <div className="px-4 w-[200px]">
+        <h2 className="text-[15px] font-medium max-h-9 truncate text-b3">
+          {product.title}
         </h2>
-        <p className="price font-medium text-[15px]">$59.99</p>
+        <p className="price font-medium text-[15px] text-center">
+          ${product.price}
+        </p>
       </div>
 
       {/* Button */}
@@ -24,7 +33,7 @@ function ProductCard(): JSX.Element {
         startIcon={<ShoppingCartIcon />}
         variant="contained"
         color="info"
-        sx={{ my: 2 }}
+        sx={{ mb: 2 }}
       >
         Add To Cart
       </Button>
