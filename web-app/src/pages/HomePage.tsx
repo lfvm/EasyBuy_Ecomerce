7f9788d41967f1d5960d59976fe9e8fb91ac4179
lfvm <lfvm200@gmail.com>
2022-11-16 09:29:27 -0600

    Added basic filtering ui

--- web-app/src/pages/HomePage.tsx
+++ web-app/src/pages/HomePage.tsx
@@ -1,3 +1,9 @@
+import {
+  FormControl,
+  FormControlLabel,
+  Radio,
+  RadioGroup,
+} from "@mui/material";
 import ErrorScreen from "../components/home/ErrorScreen";
 import ProductList from "../components/home/ProductList";
 import { useGetProducts } from "../hooks/use_fetch_products";
@@ -15,7 +21,28 @@ function HomePage() {
 
   return (
     <>
-      <div className="flex items-start justify-center">
+      <div className="flex items-center justify-center flex-col">
+        <div className="filter-bar w-full ml-auto mr-auto border bg-white rounded-lg mb-4 flex items-center justify-between p-4">
+          <h2>Filter price: </h2>
+          <FormControl>
+            <RadioGroup
+              row
+              aria-labelledby="demo-row-radio-buttons-group-label"
+              name="row-radio-buttons-group"
+            >
+              <FormControlLabel
+                value="ascending"
+                control={<Radio />}
+                label="ascending"
+              />
+              <FormControlLabel
+                value="descending"
+                control={<Radio />}
+                label="descending"
+              />
+            </RadioGroup>
+          </FormControl>
+        </div>
         <ProductList products={products!} />
       </div>
     </>
