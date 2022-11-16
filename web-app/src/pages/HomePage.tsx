9a45af2e59de4345323b3df2bcf320032a0d45c3
lfvm <lfvm200@gmail.com>
2022-11-15 19:41:21 -0600

    Renders products dinamically

--- web-app/src/pages/HomePage.tsx
+++ web-app/src/pages/HomePage.tsx
@@ -1,23 +1,20 @@
-import React from "react";
 import ProductCard from "../components/ui/ProductCard";
+import { useGetProducts } from "../hooks/use_fetch_products";
 
 function HomePage() {
+  const { products, loading } = useGetProducts();
   return (
     <>
       <div className="flex items-start justify-center">
-        <div className="grid gap-1 grid-cols-2 lg:grid-cols-4 md:grid-cols-4">
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-        </div>
+        {loading === true ? (
+          <p>Loading...</p>
+        ) : (
+          <div className="grid gap-1 grid-cols-2 lg:grid-cols-4 md:grid-cols-4">
+            {products.map((product) => (
+              <ProductCard product={product} key={product.id} />
+            ))}
+          </div>
+        )}
       </div>
     </>
   );
