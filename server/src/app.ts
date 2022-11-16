50d9c3c2fbcd6c867057f147f064433d5624f912
lfvm <lfvm200@gmail.com>
2022-11-16 16:57:18 -0600

    added stripe

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,13 +1,24 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express } from "express";
 import dotenv from "dotenv";
+import Stripe from "stripe";
 dotenv.config();
 
+//Initialize app
 const app: Express = express();
 const cors = require("cors");
 const port = process.env.PORT;
 app.use(cors());
+app.use(express.json());
 
+
+//Initialize stripe
+export const stripe = new Stripe(process.env.STRIPE_SK!, {
+  apiVersion: "2022-11-15",
+});
+
+// Routes
 app.use("/api/products", require("./routes/product_routes"));
+app.use("/api/payment-intents", require("./routes/payment_routes"));
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
