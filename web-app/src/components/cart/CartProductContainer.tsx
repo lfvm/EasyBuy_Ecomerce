acee2a9bfc0bfa4290cbd9f6bbd44d1030e6b7ea
lfvm <lfvm200@gmail.com>
2022-11-15 21:32:14 -0600

    added function to update quantity

--- web-app/src/components/cart/CartProductContainer.tsx
+++ web-app/src/components/cart/CartProductContainer.tsx
@@ -3,13 +3,44 @@ import AddIcon from "@mui/icons-material/Add";
 import { IconButton } from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
 import { CartPoduct } from "../../types/product";
+import { useAppContext } from "../../context/AppContext";
 
 interface PageLayoutInterface {
   cartProduct: CartPoduct;
 }
 
 function CartProductContainer(props: PageLayoutInterface) {
+  const { cart, setCart } = useAppContext();
   const { cartProduct } = props;
+
+  function removeItem() {
+    const newCart = cart.filter(
+      (product) => product.product.id !== cartProduct.product.id
+    );
+    setCart(newCart);
+  }
+
+  function changeAmmount(newValue: number) {
+    // Dont allow the user two have negative quantities
+    if (newValue === 0) {
+      return;
+    }
+
+    // Update the cart list with the new product ammount
+    const newCart = cart.map((item) => {
+      if (item.product.id === cartProduct.product.id) {
+        const updatedItem: CartPoduct = {
+          product: item.product,
+          quantity: newValue,
+        };
+
+        return updatedItem;
+      }
+
+      return item;
+    });
+    setCart(newCart);
+  }
 
   return (
     <div className="cart-product flex items-center justify-between p-4 border lg:w-[900px] w-[430px] bg-white rounded-md m-4 shadow-md">
@@ -18,7 +49,7 @@ function CartProductContainer(props: PageLayoutInterface) {
         <img
           src={cartProduct.product.image ?? ""}
           alt="product"
-          className="lg:w-[80px] lg:h-[80px] h-[40px] w-[40px] object-contain ml-1"
+          className="lg:w-[80px] lg:h-[80px] h-[40px] w-[40px] object-contain mx-1"
         />
 
         <h2 className="text-[10px] lg:text-[15px] w-[80px] lg:w-[230px] font-medium  text-b3 truncate">
@@ -28,22 +59,22 @@ function CartProductContainer(props: PageLayoutInterface) {
 
       {/* ammount buttons */}
       <div className="inputs flex items-center justify-center lg:border lg:border-black ">
-        <IconButton>
-          <AddIcon />
+        <IconButton onClick={() => changeAmmount(cartProduct.quantity - 1)}>
+          <RemoveIcon />
         </IconButton>
         <p className="price font-medium text-[15px] mx-4">
           {cartProduct.quantity}
         </p>
-        <IconButton>
-          <RemoveIcon />
+        <IconButton onClick={() => changeAmmount(cartProduct.quantity + 1)}>
+          <AddIcon />
         </IconButton>
       </div>
 
       {/* total-price */}
       <p className="price font-medium text-[15px] w-[20px]">
-        ${cartProduct.product.price}
+        ${cartProduct.product.price * cartProduct.quantity}
       </p>
-      <IconButton>
+      <IconButton onClick={() => removeItem()}>
         <DeleteIcon sx={{ color: "#dc2626" }} />
       </IconButton>
     </div>
