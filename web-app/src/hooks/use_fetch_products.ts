6b619375fa9cb9c4745d5fc66fb2ce7badfd9ed7
lfvm <lfvm200@gmail.com>
2022-11-15 21:53:35 -0600

    added error variable

--- web-app/src/hooks/use_fetch_products.ts
+++ web-app/src/hooks/use_fetch_products.ts
@@ -3,14 +3,16 @@ import getProducts from "../helpers/get_products";
 import { Product } from "../types/product";
 
 interface State {
-  products: Product[];
+  products?: Product[] | null;
   loading: boolean;
+  error: boolean;
 }
 
 export const useGetProducts = () => {
   const [state, setState] = useState<State>({
     products: [],
     loading: true,
+    error: false,
   });
 
   useEffect(() => {
@@ -18,6 +20,7 @@ export const useGetProducts = () => {
       setState({
         products: apiProducts,
         loading: false,
+        error: apiProducts !== null ? false : true,
       })
     );
   }, []);
