6ee313ffc266e0691abe04adae771ad013600bf9
lfvm <lfvm200@gmail.com>
2022-11-15 20:16:00 -0600

    Renders cart product data

--- web-app/src/components/cart/CartProductContainer.tsx
+++ web-app/src/components/cart/CartProductContainer.tsx
@@ -2,20 +2,27 @@ import RemoveIcon from "@mui/icons-material/Remove";
 import AddIcon from "@mui/icons-material/Add";
 import { IconButton } from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
+import { CartPoduct } from "../../types/product";
 
-function CartProductContainer() {
+interface PageLayoutInterface {
+  cartProduct: CartPoduct;
+}
+
+function CartProductContainer(props: PageLayoutInterface) {
+  const { cartProduct } = props;
+
   return (
-    <div className="cart-product flex items-center justify-between p-4 border lg:w-[800px] w-[400px] bg-white rounded-md m-4 shadow-md">
+    <div className="cart-product flex items-center justify-between p-4 border lg:w-[900px] w-[430px] bg-white rounded-md m-4 shadow-md">
       {/* Img */}
-      <div className="title flex items-center">
+      <div className="title flex items-center lg:w-[400px]">
         <img
-          src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
+          src={cartProduct.product.image ?? ""}
           alt="product"
           className="lg:w-[80px] lg:h-[80px] h-[40px] w-[40px] object-contain ml-1"
         />
 
-        <h2 className="text-[15px] font-medium max-h-9 text-b3">
-          Very Cool Product
+        <h2 className="text-[10px] lg:text-[15px] w-[80px] lg:w-[230px] font-medium  text-b3 truncate">
+          {cartProduct.product.title}
         </h2>
       </div>
 
@@ -24,14 +31,18 @@ function CartProductContainer() {
         <IconButton>
           <AddIcon />
         </IconButton>
-        <p className="price font-medium text-[15px] mx-4">1</p>
+        <p className="price font-medium text-[15px] mx-4">
+          {cartProduct.quantity}
+        </p>
         <IconButton>
           <RemoveIcon />
         </IconButton>
       </div>
 
       {/* total-price */}
-      <p className="price font-medium text-[15px]">$59.99</p>
+      <p className="price font-medium text-[15px] w-[20px]">
+        ${cartProduct.product.price}
+      </p>
       <IconButton>
         <DeleteIcon sx={{ color: "#dc2626" }} />
       </IconButton>
